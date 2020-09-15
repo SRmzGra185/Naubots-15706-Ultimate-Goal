48cdd2e2e3db938c77d179a39e6520dc17341fe0
SRmzGra185 <63324178+SRmzGra185@users.noreply.github.com>
2020-09-14 20:22:35 -0500

    Update Teleoperado.java

--- Teleoperado.java
+++ Teleoperado.java
@@ -1,1 +1,49 @@
-/*Insert code here*/
+/*Copyright 2019
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@Teleop(name= "Programa Teleoperado")
+public class Teleoperado extends OpMode {
+  public DcMotor motor;
+  public void intit("Hola", "Mundo");
+  motor = hardwareMap.get(DcMotor.class, "motor1");
+  
+  motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+  
+  motor.setDirection(DcMotor.Direction.FOWARD);
+}
+    telemetry.addData
+}
+  public void init_loop(){
+  motor.setPower(gamepad1.left_stick_y);
+  motor.setPower(-gamepad1.left_stick_y);
+  Range.clip(1, 8, 5);
+  }
+  public void start(){
+  }
+  public void loop(){
+  }
+  public void stop(){
+  }
